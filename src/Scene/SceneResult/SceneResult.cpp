a061953b9ab0138c02372764261ad6a550fe8a1a
ster1271 <144640199+ster1271@users.noreply.github.com>
2024-04-25 17:14:58 +0900

    リザルトシーンの枠組み追加
    プレイシーンもリザルトシーンもシーン移動は
    できるようにしました

--- src/Scene/SceneResult/SceneResult.cpp
+++ src/Scene/SceneResult/SceneResult.cpp
@@ -1,5 +1,7 @@
 #include "DxLib.h"
 #include "SceneResult.h"
+#include "../../Input/Input.h"
+#include "../../Common.h"
 
 void SceneResult::Init()
 {
@@ -7,9 +9,13 @@ void SceneResult::Init()
 }
 void SceneResult::Step()
 {
-
+	if (Input::IsKeyPush(KEY_INPUT_RETURN))
+	{
+		g_current_scene_ID = Title;
+	}
 }
 void SceneResult::Draw()
 {
-
+	DrawString(0, 0, "リザルトシーンです。", GetColor(255, 255, 255), true);
+	DrawString(0, 15, "Enterで次のシーンへ", GetColor(255, 255, 255), true);
 }
